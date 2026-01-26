e9457731094eead1b40809e814f1d1a316cb2a1b
manjurai22 <manjurai611@gmail.com>
2026-01-26 22:14:39 +0545

    api testing for users and group -CRUD

--- api/urls.py
+++ api/urls.py
@@ -6,7 +6,8 @@ from api import views
 router = routers.DefaultRouter()
 router.register(r"users", views.UserViewSet)
 router.register(r"groups", views.GroupViewSet)
-
+router.register(r"tags", views.TagViewSet)
+router.register(r"category", views.CategoryViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
