f19f6c329a92154447452a85e35419f8f91601b3
manjurai22 <manjurai611@gmail.com>
2026-01-28 23:51:36 +0545

    simplify serializers for postman testing using IDs inplace of urls

--- api/serializers.py
+++ api/serializers.py
@@ -10,13 +10,13 @@ from newspaper.models import Category
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ["url", "username", "email", "groups"]
+        fields = ["id", "username", "email", "groups"]
 
 
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Group
-        fields = ["url", "name"]
+        fields = ["id", "name"]
 
 class TagSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
